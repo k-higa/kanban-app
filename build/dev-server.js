024968783fc0e50d3b7e2c2cbd0f5b3631303cd6
比嘉　和之 <w-_-waaa1@i.softbank.jp>
2018-10-27 15:12:01 +0900

    add Login

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,7 +1,7 @@
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser")
 
 module.exports = app => {
-    app.use(bodyParser.json())
+  app.use(bodyParser.json())
 }
 
 //TODO add mock json
