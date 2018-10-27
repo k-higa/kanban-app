73710d13a49719dce2c38bbe8be053062eb559aa
比嘉　和之 <w-_-waaa1@i.softbank.jp>
2018-10-27 13:54:19 +0900

    add api

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import AUth from './auth'
+import List from './list'
+import Task from './task'
+
+export { AUth, List, Task }
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,3 @@
-// /*eslint-disable no-unused-vars */
 import * as types from './mutation-types'
 
 export default {
