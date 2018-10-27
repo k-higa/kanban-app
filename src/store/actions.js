57c786e2362382fe5d11ab918906a3e692f74231
比嘉　和之 <w-_-waaa1@i.softbank.jp>
2018-10-27 13:52:00 +0900

    add action

---
+++ src/store/actions.js
@@ -0,0 +1,29 @@
+// /*eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+
+export default {
+  login: ({ commit }) => {
+    // TODO
+    throw new Error('login action should be implemented')
+  },
+  fetchList: ({ commit }) => {
+    // TODO
+    throw new Error('fetchList action should be implementd')
+  },
+  addTask: ({ commit }) => {
+    // TODO
+    throw new Error('addTask action should be implmented')
+  },
+  updateTask: ({ commit }) => {
+    // TODO
+    throw new Error('updateTask action should be implmented')
+  },
+  removeTask: ({ commit }) => {
+    // TODO
+    throw new Error('removeTask action should be implmented')
+  },
+  logout: ({ commit }) => {
+    // TODO
+    throw new Error('logout action should be implemented')
+  }
+}
